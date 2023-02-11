b70dd8ebf186d05afb271c6f2816bfc99e5e0b42
m-malanowski <info@mateusz-malanowski.pl>
2023-02-11 22:10:08 +0100

    marqee

--- src/components/horizontalText.js
+++ src/components/horizontalText.js
@@ -1,10 +1,38 @@
-import * as React from "react"
+
+import React, { useRef, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 
 const HorizontalText = () => {
+
+    const textRef = useRef(null);
+    const textRefLeft = useRef(null);
+
+    const { transform } = useSpring({
+        transform: `translateX(${textRef.current ? -textRef.current.getBoundingClientRect().left : 0}px)`,
+        from: { transform: "translateX(0)" }
+    });
+
+    const { transformLeft } = useSpring({
+        transform: `translateX(${textRefLeft.current ? -textRefLeft.current.getBoundingClientRect().left : 0}px)`,
+        from: { transform: "translateX(0)" }
+    });
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            let foo = window.scrollY
+            textRef.current.style.transform = `translateX(${foo * 0.1 }px)`;
+            textRefLeft.current.style.transform = `translateX(${-(foo * 0.2 )}px)`;
+        });
+
+        return () => {
+            window.removeEventListener("scroll", () => {});
+        };
+    }, []);
+
     return (
         <div className="section horizontal-text">
-            <h2>Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183;</h2>
-            <h2>Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183;</h2>
+            <animated.h2 style={{ transform }} ref={textRef}>Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183;</animated.h2>
+            <animated.h2 style={{ transformLeft }} ref={textRefLeft}> Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183; Wyraź Swoje poparcie &#183;</animated.h2>
         </div>
     )
 }
